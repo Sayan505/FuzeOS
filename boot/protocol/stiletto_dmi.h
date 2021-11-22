bcfa55b162fa947d904ead9563f5e8e949d8dd37
Sayan Bhattacharjee <bsayan8086@gmail.com>
2021-11-22 20:41:15 +0530

    write boot protocols for DMI

--- boot/protocol/stiletto_dmi.h
+++ boot/protocol/stiletto_dmi.h
@@ -5,5 +5,29 @@
 
 
 typedef struct {
-    char ProcessorVersion[64];
+    stiletto_dmi_type2_baseboard_t baseboard;
+    stiletto_dmi_type4_processor_t processor;
+    stiletto_dmi_type17_memory_a_t memory_a;
+    stiletto_dmi_type20_memory_b_t memory_b;
 } stiletto_dmi_t;
+
+
+typedef struct {
+    char Manufacturer[64];
+    char ProductName[64];
+} stiletto_dmi_type2_baseboard_t;
+
+typedef struct {
+    char           ProcessorVersion[64];
+    unsigned short CurrentSpeed;
+    unsigned char  CoreCount;
+    unsigned char  ThreadCount;
+} stiletto_dmi_type4_processor_t;
+
+typedef struct {
+    unsigned short ConfiguredMemoryClockSpeed;
+} stiletto_dmi_type17_memory_a_t;
+
+typedef struct {
+    unsigned long long Size;    // ExtendedEndingAddress - ExtendedStartingAddress + 1 (Bytes)
+} stiletto_dmi_type20_memory_b_t;
---
+++ kernel/sys/bootprotocol/stiletto_dmi.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include <boot/protocol/stiletto_dmi.h>>
