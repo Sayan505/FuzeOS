d548785d253ac929369dbcd74237264ef7861366
Sayan Bhattacharjee <bsayan8086@gmail.com>
2022-02-27 19:59:52 +0530

    GDT (wip)

--- kernel/sys/gdt64/gdt64.c
+++ kernel/sys/gdt64/gdt64.c
@@ -1,64 +1,49 @@
 #include <sys/gdt64/gdt64.h>
-#include <sys/gdt64/tss64.h>
 
 
-__attribute__((unused))
-static VOID* zero_memory(VOID *dest, UINT64 n_bytes) {
-	BYTE *p = (BYTE *)dest;
-
-	while (n_bytes--)
-        *(p++) = NULL;
-
-	return dest;
+void set_segments(UINT16 cs, UINT16 ds) {
+    __asm__ volatile(
+        "push %0\n\t"
+        "push $1f\n\t"
+        "lretq\n\t"
+        "1:\n\t"
+        "mov %1, %%ds\n\t"
+        "mov %1, %%es\n\t"
+        "mov %1, %%fs\n\t"
+        "mov %1, %%gs\n\t"
+        "cli\n\t"               // disable interrupts
+        "mov %1, %%ss\n\t"
+        "sti\n\t"               // enable interrupts
+        :: "rmi"((uint64_t)cs), "rm"((uint64_t)ds)
+        : "memory"
+    );
 }
-
 
 void init_gdt() {
     // populate GDT
     gdt_t gdt = {
         {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // struct gdt_entry null;
 
-        {0x00, 0x00, 0x00, 0x9A, 0xA0, 0x00}, // struct gdt_entry kernel_cs;
-        {0x00, 0x00, 0x00, 0x92, 0xA0, 0x00}, // struct gdt_entry kernel_ds;
-
-        {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // struct gdt_entry user_null;
-        {0x00, 0x00, 0x00, 0x9A, 0xA0, 0x00}, // struct gdt_entry user_cs;
-        {0x00, 0x00, 0x00, 0x92, 0xA0, 0x00}, // struct gdt_entry user_ds;
-        
-        {0x00, 0x00, 0x00, 0x9A, 0xA0, 0x00}, // struct gdt_entry fw_cs;
-        {0x00, 0x00, 0x00, 0x92, 0xA0, 0x00}, // struct gdt_entry fw_ds;
-        
-        {0x00, 0x00, 0x00, 0x89, 0xA0, 0x00}, // struct gdt_entry tss_low;
-        {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}  // struct gdt_entry tss_high;
+        {0x00, 0x00, 0x00, 0x9A, 0x20, 0x00}, // struct gdt_entry kernel_cs;
+        {0x00, 0x00, 0x00, 0x92, 0x00, 0x00}, // struct gdt_entry kernel_ds;
     };
 
-
-    // prep Task State Segment
-    tss_t tss;
-    zero_memory((void *)&tss, sizeof(tss_t));
-
-    UINT64 tss_base = (UINT64)&tss;    // fetch TSS base addr
-
-    gdt.tss_low.base15_0  =  tss_base & 0xFFFF;
-    gdt.tss_low.base23_16 = (tss_base >> 16) & 0xFF;
-    gdt.tss_low.base31_24 = (tss_base >> 24) & 0xFF;
-    gdt.tss_low.limit15_0 = sizeof(tss_t);
-
-    gdt.tss_high.limit15_0 = (tss_base >> 32) & 0xFFFF;
-    gdt.tss_high.base15_0  = (tss_base >> 48) & 0xFFFF;
     
     // init gdt
     gdt_desc_t gdt_desc;
 
-    gdt_desc.limit = sizeof(gdt_t) - 1;    // set size
-    gdt_desc.base  = (UINT64)&gdt;         // pass populated gdt
+    gdt_desc.limit = (UINT16)(sizeof(gdt_t) - 1);    // set size
+    gdt_desc.base  = (UINT64)(&gdt);                 // pass populated gdt
 
-    LOAD_GDT64(&gdt_desc);    // upload gdt to cpu
+
+    __asm__ volatile("lgdt %0" :: "m"(gdt_desc) : "memory"); // upload gdt to cpu
+    set_segments(0x08, 0x10);
 }
 
 
 
 
+/*
 // supply memcpy to make LD stop complaining
 __attribute__((unused))
 VOID* memcpy(VOID* dest, const VOID* src, UINT64 n_bytes) {
@@ -74,3 +59,4 @@ VOID* memcpy(VOID* dest, const VOID* src, UINT64 n_bytes) {
     }
     return dest;
 }
+*/
--- kernel/sys/gdt64/gdt64.h
+++ kernel/sys/gdt64/gdt64.h
@@ -14,39 +14,28 @@ typedef struct tag_gdt_desc {
 
 
 typedef struct tag_gdt_entry {
-    UINT16 limit15_0;
-    UINT16 base15_0;
+    UINT16 limit_low_15_0;
+    UINT16 base_low_15_0;
 
-    UINT8 base23_16;
+    UINT8 base_mid_23_16;
     UINT8 access_byte;
-    UINT8 limit19_16_and_flags;
+    UINT8 granularity;
 
-    UINT8 base31_24;
+    UINT8 base_high_31_24;
 } gdt_entry_t;
 
 
-__attribute__((aligned(0x1000)))
 typedef struct tag_gdt {
     gdt_entry_t null;
 
     gdt_entry_t kernel_cs;
     gdt_entry_t kernel_ds;
-
-    gdt_entry_t user_null;
-    gdt_entry_t user_cs;
-    gdt_entry_t user_ds;
-
-    gdt_entry_t fw_cs;
-    gdt_entry_t fw_ds;
-
-    gdt_entry_t tss_low;
-    gdt_entry_t tss_high;
 } gdt_t;
 
 
 #pragma pack()
 
 
-extern void LOAD_GDT64(gdt_desc_t* gdt_desc);   // kernel/sys/gdt64/lgdt64.asm
+void set_segments(UINT16 cs, UINT16 ds);
 
 void init_gdt();
